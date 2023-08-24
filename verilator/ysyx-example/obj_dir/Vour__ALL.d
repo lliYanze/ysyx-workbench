266409fdff51ce7df397af60e82268064b1c4a09
李彦泽 <@1310469009@qq.com>
2023-08-24 20:15:38 +0800

    双控开关波形显示 down

---
+++ verilator/ysyx-example/obj_dir/Vour__ALL.d
@@ -0,0 +1,15 @@
+Vour__ALL.o: Vour__ALL.cpp Vour.cpp Vour.h \
+ /usr/local/share/verilator/include/verilated.h \
+ /usr/local/share/verilator/include/verilatedos.h \
+ /usr/local/share/verilator/include/verilated_config.h \
+ /usr/local/share/verilator/include/verilated_types.h \
+ /usr/local/share/verilator/include/verilated_funcs.h Vour__Syms.h \
+ /usr/local/share/verilator/include/verilated_vcd_c.h \
+ /usr/local/share/verilator/include/verilated.h \
+ /usr/local/share/verilator/include/verilated_trace.h \
+ /usr/local/share/verilator/include/verilated_trace_defs.h \
+ Vour___024root.h Vour___024root__DepSet_hf7027e39__0.cpp \
+ Vour___024root__DepSet_h637983f1__0.cpp Vour__Trace__0.cpp \
+ Vour___024root__Slow.cpp Vour___024root__DepSet_hf7027e39__0__Slow.cpp \
+ Vour___024root__DepSet_h637983f1__0__Slow.cpp Vour__Syms.cpp \
+ Vour__Trace__0__Slow.cpp
---
+++ verilator/ysyx-example/obj_dir/Vour___024root__DepSet_hf7027e39__0__Slow.cpp
@@ -0,0 +1,47 @@
+// Verilated -*- C++ -*-
+// DESCRIPTION: Verilator output: Design implementation internals
+// See Vour.h for the primary calling header
+
+#include "verilated.h"
+
+#include "Vour__Syms.h"
+#include "Vour___024root.h"
+
+VL_ATTR_COLD void Vour___024root___eval_initial__TOP(Vour___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vour__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vour___024root___eval_initial__TOP\n"); );
+    // Init
+    VlWide<5>/*159:0*/ __Vtemp_hfa101174__0;
+    // Body
+    if (VL_UNLIKELY((0U != VL_TESTPLUSARGS_I(std::string{"trace"})))) {
+        __Vtemp_hfa101174__0[0U] = 0x2e766364U;
+        __Vtemp_hfa101174__0[1U] = 0x64756d70U;
+        __Vtemp_hfa101174__0[2U] = 0x766c745fU;
+        __Vtemp_hfa101174__0[3U] = 0x6f67732fU;
+        __Vtemp_hfa101174__0[4U] = 0x6cU;
+        vlSymsp->_vm_contextp__->dumpfile(VL_CVT_PACK_STR_NW(5, __Vtemp_hfa101174__0));
+        vlSymsp->_traceDumpOpen();
+        VL_WRITEF("[%0t] Tracing to logs/vlt_dump.vcd...\n\n",
+                  64,VL_TIME_UNITED_Q(1),-12);
+    }
+    VL_WRITEF("[%0t] Model running...\n\n",64,VL_TIME_UNITED_Q(1),
+              -12);
+}
+
+#ifdef VL_DEBUG
+VL_ATTR_COLD void Vour___024root___dump_triggers__stl(Vour___024root* vlSelf);
+#endif  // VL_DEBUG
+
+VL_ATTR_COLD void Vour___024root___eval_triggers__stl(Vour___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vour__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vour___024root___eval_triggers__stl\n"); );
+    // Body
+    vlSelf->__VstlTriggered.at(0U) = (0U == vlSelf->__VstlIterCount);
+#ifdef VL_DEBUG
+    if (VL_UNLIKELY(vlSymsp->_vm_contextp__->debug())) {
+        Vour___024root___dump_triggers__stl(vlSelf);
+    }
+#endif
+}
