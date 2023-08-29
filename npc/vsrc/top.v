692d5cbd576a16e5697334328cfcb664e731ddac
tracer-ysyx <tracer@ysyx.org>
2023-08-29 22:52:00 +0800

    > sim RTL ysyx_22040000 李彦泽 Linux alan-virtual-machine 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:52:00 up 1 day, 16:11,  5 users,  load average: 0.88, 0.51, 0.33

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -29,7 +29,7 @@ module top (
     //.ledr(ledr)
 //);
 
-//assign VGA_CLK = clk;
+assign VGA_CLK = clk;
 
 //wire [9:0] h_addr;
 //wire [9:0] v_addr;
@@ -49,25 +49,25 @@ module top (
     //.vga_b(VGA_B)
 //);
 
-ps2_keyboard my_keyboard(
-    .clk(clk),
-    .resetn(~rst),
-    .ps2_clk(ps2_clk),
-    .ps2_data(ps2_data)
-);
-
-//seg mu_seg(
+//ps2_keyboard my_keyboard(
     //.clk(clk),
-    //.rst(rst),
-    //.o_seg0(seg0),
-    //.o_seg1(seg1),
-    //.o_seg2(seg2),
-    //.o_seg3(seg3),
-    //.o_seg4(seg4),
-    //.o_seg5(seg5),
-    //.o_seg6(seg6),
-    //.o_seg7(seg7)
+    //.resetn(~rst),
+    //.ps2_clk(ps2_clk),
+    //.ps2_data(ps2_data)
 //);
+
+seg mu_seg(
+    .clk(clk),
+    .rst(rst),
+    .o_seg0(seg0),
+    .o_seg1(seg1),
+    .o_seg2(seg2),
+    .o_seg3(seg3),
+    .o_seg4(seg4),
+    .o_seg5(seg5),
+    .o_seg6(seg6),
+    .o_seg7(seg7)
+);
 
 //vmem my_vmem(
     //.h_addr(h_addr),
