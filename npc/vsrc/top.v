efa71aa12ce385de6637c48a3fed39b62078492a
tracer-ysyx <tracer@ysyx.org>
2023-08-31 22:29:18 +0800

    > sim RTL ysyx_22040000 李彦泽 Linux alan-virtual-machine 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:29:18 up 2 days,  6:57,  6 users,  load average: 0.39, 0.37, 0.35

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -5,11 +5,11 @@ module top(
 );
 reg[31:0] count;
 
-always @( !clk) begin
+always @(posedge clk) begin
     if(rst) begin ledr <= 1; count <= 0; end
     else begin 
         if(count == 0) ledr <= {ledr[14:0], ledr[15]};
-        count <= (count >= 5000 ? 32'b0 : count+1);
+        count <= (count >= 3 ? 32'b0 : count+1);
         end
     end
 
