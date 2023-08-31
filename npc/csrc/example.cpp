440e84adc43b58cb1599cfa8a982895f9243e3d5
tracer-ysyx <tracer@ysyx.org>
2023-08-31 22:18:46 +0800

    > sim RTL ysyx_22040000 李彦泽 Linux alan-virtual-machine 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:18:46 up 2 days,  6:47,  6 users,  load average: 0.22, 0.25, 0.26

--- npc/csrc/example.cpp
+++ npc/csrc/example.cpp
@@ -1,29 +1,29 @@
-//#include <stdio.h>
-//#include <stdlib.h>
-//#include <assert.h>
 #include <Vtop.h>
 #include "verilated.h"
-//#include <verilated_vcd_c.h>
 #include <nvboard.h>
 
 void nvboard_bind_all_pins(Vtop* top);
 
 static TOP_NAME dut;
+
+void single_cycle() {
+    dut.clk = 0; dut.eval();
+    dut.clk = 1; dut.eval();
+}
+
+void reset(int times) {
+    dut.rst = 0;
+    while(times--) single_cycle();
+}
+
+
 int main(int arg, char** argv){
-
-    
-
   nvboard_bind_all_pins(&dut);
   nvboard_init();
-
-  //reset(10);
+  reset(10);
 
   while(1) {
     nvboard_update();
-    //int a = rand() & 1;
-    //int b = rand() & 1;
-    //dut.a = a;
-    //dut.b = b;
     dut.eval();
   }
 }
--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -1,7 +1,16 @@
 module top(
-    input a,
-    input b,
-    output f
+    input clk,
+    input rst,
+    output reg[15:0] ledr
 );
-    assign f = a ^ b;
+reg[31:0] count;
+
+always @(posedge clk) begin
+    if(rst) begin ledr <=1; count <= 0; end
+    else begin 
+        if(count == 0) ledr <= {ledr[14:0], ledr[15]};
+        count <= (count >= 5000000 ? 32'b0 : count+1);
+        end
+    end
+
 endmodule
