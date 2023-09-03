849c68147ee531db17d4a3d5b518959abf67b5a5
李彦泽 <@1310469009@qq.com>
2023-09-03 22:41:05 +0800

    数电实验2

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -1,21 +1,47 @@
 module top(
-    input wire[1:0] Y,
-    input wire[1:0] X0,
-    input wire[1:0] X1,
-    input wire[1:0] X2,
-    input wire[1:0] X3,
-    output reg[1:0] F
+    input wire[7:0] sw,
+    input wire sw_enable,
+    output reg no_input,
+    output reg [2:0] ledr,
+    output reg[7:0] seg0
     
 );
 
+wire [2:0] num;
 
-always @(Y or X0 or X1 or X2 or X3) begin
-    case(Y) 
-        2'b00: F = X0;
-        2'b01: F = X1;
-        2'b10: F = X2;
-        2'b11: F = X3;
-        default: F = 2'b0;
-    endcase
+
+tran_8to3 tran1(.tran_input_wire(sw),
+           .tran_enable(sw_enable),
+           .tran_all_down(no_input),
+           .tran_output_wire(num)
+);
+
+digital_led digital_led1(.input_num(num),
+    .output_seg(seg0)
+);
+
+
+always @(num) begin
+    if(num[0] == 1'b1) begin
+        ledr[0] = 1;
     end
+    else begin
+        ledr[0] = 0;
+    end
+    if(num[1] == 1'b1) begin
+        ledr[1] = 1;
+    end
+    else begin
+        ledr[1] = 0;
+    end
+    if(num[2] == 1'b1) begin
+        ledr[2] = 1;
+    end
+    else begin
+        ledr[2] = 0;
+    end
+end
+
 endmodule
+
+
