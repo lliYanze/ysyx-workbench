2410700ed448e330c550ccff201934ef0840f7a7
李彦泽 <@1310469009@qq.com>
2023-09-05 21:46:22 +0800

    数电实验3 - 实现了与

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -8,8 +8,9 @@ module top(
     output reg overflow
 );
 
-reg [3:0] B_complement;
+reg [3:0] B_complement = 4'b0000;
 always @(A or B or mod) begin
+    B_complement = 4'b0000;
     case(mod)
         //add
         3'b000: begin 
@@ -21,18 +22,27 @@ always @(A or B or mod) begin
         3'b001: begin
             B_complement = ~B + 1;
             {C,result} = A + B_complement;  
+            //{C,result} = A + ~B + 1;  
             overflow = (A[3] == B_complement[3])&(A[3] != result[3]);
+            //overflow = (A[3] == [3])&(A[3] != result[3]);
             zero = ~(|result);
 
         end
         //取反
         3'b010: begin
-            if(A[3]) begin
+            zero = 0;
+            overflow = 0;
+            C = 0;
+            if(A[3]) 
                 result = {A[3],~A[2:0]};
-            end 
-            else begin
+            else 
                 result = A;
-            end
+        end
+        3'b011: begin
+            result = A & B;
+            zero = 0;
+            overflow = 0;
+            C = 0;
         end
         default: begin C = 1'b0; zero = 1'b0;result = 4'b0000;overflow = 1'b0;end
     endcase
