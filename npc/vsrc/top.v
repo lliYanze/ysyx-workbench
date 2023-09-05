cba0139bedc5b45a592c8989915621110527e4fe
李彦泽 <@1310469009@qq.com>
2023-09-05 21:15:42 +0800

    数电实验3 - 实现了sub

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -8,9 +8,23 @@ module top(
     output reg overflow
 );
 
+reg [3:0] B_complement;
 always @(A or B or mod) begin
     case(mod)
-        3'b000: begin {C,result} = A + B;  end //add
+        //add
+        3'b000: begin 
+            {C,result} = A + B;  
+            overflow = (A[3] == B[3])&(A[3] != result[3]);
+            zero = ~(|result);
+        end 
+        //sub
+        3'b001: begin
+            B_complement = ~B + 1;
+            {C,result} = A + B_complement;  
+            overflow = (A[3] == B_complement[3])&(A[3] != result[3]);
+            zero = ~(|result);
+
+        end
         default: begin C = 1'b0; zero = 1'b0;result = 4'b0000;overflow = 1'b0;end
     endcase
 end
