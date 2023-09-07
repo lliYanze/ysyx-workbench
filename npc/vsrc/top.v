daab49a5ed7de48264de3a2b7571c7547485c838
李彦泽 <@1310469009@qq.com>
2023-09-07 21:06:18 +0800

    数电实验4 - 实现随机序列

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -1,5 +1,5 @@
 module top(
-    input wire clk,
+    input wire sw_clk,
     output reg[7:0] seg_led0,
     output reg[7:0] seg_led1,
     output reg[7:0] ledr
@@ -9,9 +9,17 @@ reg [7:0] random_num;
 
 seg seg0(.input_num(random_num[3:0]), .output_seg(seg_led0));
 seg seg1(.input_num(random_num[7:4]), .output_seg(seg_led1));
+reg C;
 
-assign random_num = 8'b00010111;
 
+always @(posedge sw_clk) begin
+    if(random_num == 8'b00000000) 
+        random_num <= 8'b00100000;
+    else begin
+        C <= ^random_num[4:1]^random_num[0];
+        random_num <= {C, random_num[7:1]};
+    end
+end
 
 always @(random_num) begin
     ledr = random_num;
