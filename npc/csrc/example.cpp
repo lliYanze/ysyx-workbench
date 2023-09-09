43ae5982726ee949e7148c4d86df98f24a1e590e
李彦泽 <@1310469009@qq.com>
2023-09-09 21:53:52 +0800

    数电实验7 -- 完成验收部分

--- npc/csrc/example.cpp
+++ npc/csrc/example.cpp
@@ -1,17 +1,40 @@
 #include <Vtop.h>
-#include "verilated.h"
+#include <verilated.h>
 #include <nvboard.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <verilated_vcd_c.h>
 
 void nvboard_bind_all_pins(Vtop* top);
 
 static TOP_NAME dut;
 
-int main(int arg, char** argv){
-  nvboard_bind_all_pins(&dut);
-  nvboard_init();
+VerilatedVcdC *m_trace = new VerilatedVcdC;
 
-  while(1) {
-    nvboard_update();
-    dut.eval();
+int sim_time = 0;
+
+static void single_cycle() {
+  dut.clk = 0; dut.eval(); m_trace->dump(sim_time); sim_time++;
+  dut.clk = 1; dut.eval(); m_trace->dump(sim_time); sim_time++;
+}
+
+static void reset(int n) {
+  dut.ret = 1;
+  while (n -- > 0) single_cycle();
+  dut.ret = 0;
+}
+
+int main(int arg, char** argv){
+    Verilated::mkdir("logs");
+    Verilated::traceEverOn(true);
+    dut.trace(m_trace, 5);
+    m_trace->open("./logs/key.vcd");
+    nvboard_bind_all_pins(&dut);
+    nvboard_init();
+
+    while(1) {
+        nvboard_update();
+        single_cycle();
     }
 }
--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -1,29 +1,57 @@
 module top(
-    input wire sw_clk,
+    input wire clk,
+    input wire ret,
+    input wire ps2_clk,
+    input wire ps2_data,
     output reg[7:0] seg_led0,
     output reg[7:0] seg_led1,
-    output reg[7:0] ledr
+    output reg[7:0] seg_led2,
+    output reg[7:0] seg_led3,
+    output reg[7:0] seg_led4,
+    output reg[7:0] seg_led5
 );
 
-reg [7:0] random_num;
+reg [7:0] buffer;
 
-seg seg0(.input_num(random_num[3:0]), .output_seg(seg_led0));
-seg seg1(.input_num(random_num[7:4]), .output_seg(seg_led1));
-reg C;
+reg  key_clk;
+reg [7:0] num_count;
+reg [7:0] asc;
 
+key key0(.clk(clk), .resetn(~ret), .ps2_clk(ps2_clk), .ps2_data(ps2_data), .out_buffer(buffer), .key_clk(key_clk), .num_count(num_count));
 
-always @(posedge sw_clk) begin
-    if(random_num == 8'b00000000) 
-        random_num <= 8'b00100000;
-    else begin
-        C <= ^random_num[4:1]^random_num[0];
-        random_num <= {C, random_num[7:1]};
-    end
-end
+vmem vmem0(.clk(key_clk), .addr(buffer), .asc(asc));
 
-always @(random_num) begin
-    ledr = random_num;
-end
+seg seg0(.clk(key_clk), .input_num(buffer[3:0]), .output_seg(seg_led0));
+seg seg1(.clk(key_clk), .input_num(buffer[7:4]), .output_seg(seg_led1));
+seg seg4(.clk(key_clk), .input_num(num_count[3:0]), .output_seg(seg_led4));
+seg seg5(.clk(key_clk), .input_num(num_count[7:4]), .output_seg(seg_led5));
+
+seg seg2(.clk(key_clk), .input_num(asc[3:0]), .output_seg(seg_led2));
+seg seg3(.clk(key_clk), .input_num(asc[7:4]), .output_seg(seg_led3));
 
 
 endmodule
+
+
+
+module vmem(clk, addr, asc);
+input clk;
+input reg[7:0] addr;
+output reg[7:0] asc;
+
+reg [7:0] mem[255:0];
+
+
+initial begin
+    mem[8'h1c] = 8'h41;
+    mem[8'h32] = 8'h42;
+    mem[8'h21] = 8'h43;
+    mem[8'h23] = 8'h44;
+    mem[8'h24] = 8'h45;
+end
+
+assign   asc = mem[addr];
+
+endmodule
+
+
