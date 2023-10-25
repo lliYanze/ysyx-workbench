9518da52f8d68813c98787ac14738866489fddb9
李彦泽 <@1310469009@qq.com>
2023-10-25 20:18:42 +0800

    make count可以输出行数

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -45,6 +45,10 @@ gdb: run-env
 	$(call git_commit, "gdb NEMU")
 	gdb -s $(BINARY) --args $(NEMU_EXEC)
 
+count: 
+	@echo "Total lines of code:"
+	@find . -name "*.[ch]" | xargs egrep -v '^$$' | wc -l
+
 clean-tools = $(dir $(shell find ./tools -maxdepth 2 -mindepth 2 -name "Makefile"))
 $(clean-tools):
 	-@$(MAKE) -s -C $@ clean
