a0d576a0b0ff87f6a66579c874d47a7445db976c
李彦泽 <@1310469009@qq.com>
2023-11-12 00:01:53 +0800

    完成iringbuf

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -1,22 +1,23 @@
 /***************************************************************************************
-* Copyright (c) 2014-2022 Zihao Yu, Nanjing University
-*
-* NEMU is licensed under Mulan PSL v2.
-* You can use this software according to the terms and conditions of the Mulan PSL v2.
-* You may obtain a copy of Mulan PSL v2 at:
-*          http://license.coscl.org.cn/MulanPSL2
-*
-* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
-* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
-* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
-*
-* See the Mulan PSL v2 for more details.
-***************************************************************************************/
+ * Copyright (c) 2014-2022 Zihao Yu, Nanjing University
+ *
+ * NEMU is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan
+ *PSL v2. You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY
+ *KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ *NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ * See the Mulan PSL v2 for more details.
+ ***************************************************************************************/
 
 #include <cpu/cpu.h>
 #include <cpu/decode.h>
 #include <cpu/difftest.h>
 #include <locale.h>
+#include <memory/vaddr.h>
 
 #include "../monitor/sdb/sdb.h"
 
@@ -27,6 +28,55 @@
  */
 #define MAX_INST_TO_PRINT 10
 
+//环形缓冲区
+#define IRING_BUF_SIZE 20
+static char itrace_buf[IRING_BUF_SIZE][128] = {};
+static unsigned int idx = 0;
+
+void one_instruction_decode(vaddr_t pc) {
+  char *p = itrace_buf[idx];
+  uint32_t code = vaddr_ifetch(pc, 4);
+  uint8_t *inst = (uint8_t *)&code;
+  p += snprintf(p, sizeof(itrace_buf[idx]), "--> 0x%08" PRIx32 ":", pc);
+  for (int i = 3; i >= 0; i--) {
+    p += snprintf(p, 4, " %02x", inst[i]);
+  }
+  int ilen = 4;
+  /*int i;*/
+  /*for (i = ilen - 1; i >= 0; i--) {*/
+  /*p += snprintf(p, 4, " %02x", inst[i]);*/
+  /*}*/
+  int ilen_max = MUXDEF(CONFIG_ISA_x86, 8, 4);
+  int space_len = ilen_max - ilen;
+  if (space_len < 0)
+    space_len = 0;
+  space_len = space_len * 3 + 1;
+  memset(p, ' ', space_len);
+  p += space_len;
+
+  void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
+  disassemble(p, itrace_buf[idx] + sizeof(itrace_buf[idx]) - p, pc,
+              (uint8_t *)&code, ilen);
+}
+
+void iringbuf_push(char *str) {
+  Assert(strlen(str) < 128, "str is too long");
+  Assert(str != NULL, "str is NULL");
+  strcpy(itrace_buf[idx], str);
+  idx = (idx + 1) % IRING_BUF_SIZE;
+}
+void iringbuf_pop() {
+  unsigned int target_idx = idx;
+  idx = (idx + 1) % IRING_BUF_SIZE;
+  while (target_idx != idx) {
+    puts(itrace_buf[idx]);
+    idx = (idx + 1) % IRING_BUF_SIZE;
+  }
+  one_instruction_decode(cpu.pc);
+  puts(itrace_buf[idx]);
+  return;
+}
+
 CPU_state cpu = {};
 uint64_t g_nr_guest_inst = 0;
 static uint64_t g_timer = 0; // unit: us
@@ -35,22 +85,28 @@ static bool g_print_step = false;
 void device_update();
 
 static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
-#ifdef CONFIG_ITRACE_COND
-    if (ITRACE_COND) { log_write("%s\n", _this->logbuf); }
-#endif
-    if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
-    IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
+//放入循环缓冲区
+  iringbuf_push(_this->logbuf);
 
-//TODO: scan watchpoint
-    word_t result = check_wp();
-    if(result == NOT_HIT){
-        return;
-    }
-    else{
-        printf("Hit watchpoint %d at address 0x%x\n", result, cpu.pc);
-        nemu_state.state = NEMU_STOP;
-    }
-    
+#ifdef CONFIG_ITRACE_COND
+  if (ITRACE_COND) {
+    log_write("%s\n", _this->logbuf);
+  }
+#endif
+
+  if (g_print_step) {
+    IFDEF(CONFIG_ITRACE, puts(_this->logbuf));
+  } //一次执行过多指令就不会打印在终端上
+  IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
+
+  // TODO: scan watchpoint
+  word_t result = check_wp();
+  if (result == NOT_HIT) {
+    return;
+  } else {
+    printf("Hit watchpoint %d at address 0x%x\n", result, cpu.pc);
+    nemu_state.state = NEMU_STOP;
+  }
 }
 
 static void exec_once(Decode *s, vaddr_t pc) {
@@ -64,12 +120,13 @@ static void exec_once(Decode *s, vaddr_t pc) {
   int ilen = s->snpc - s->pc;
   int i;
   uint8_t *inst = (uint8_t *)&s->isa.inst.val;
-  for (i = ilen - 1; i >= 0; i --) {
+  for (i = ilen - 1; i >= 0; i--) {
     p += snprintf(p, 4, " %02x", inst[i]);
   }
   int ilen_max = MUXDEF(CONFIG_ISA_x86, 8, 4);
   int space_len = ilen_max - ilen;
-  if (space_len < 0) space_len = 0;
+  if (space_len < 0)
+    space_len = 0;
   space_len = space_len * 3 + 1;
   memset(p, ' ', space_len);
   p += space_len;
@@ -77,7 +134,8 @@ static void exec_once(Decode *s, vaddr_t pc) {
 #ifndef CONFIG_ISA_loongarch32r
   void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
   disassemble(p, s->logbuf + sizeof(s->logbuf) - p,
-      MUXDEF(CONFIG_ISA_x86, s->snpc, s->pc), (uint8_t *)&s->isa.inst.val, ilen);
+              MUXDEF(CONFIG_ISA_x86, s->snpc, s->pc),
+              (uint8_t *)&s->isa.inst.val, ilen);
 #else
   p[0] = '\0'; // the upstream llvm does not support loongarch32r
 #endif
@@ -86,11 +144,12 @@ static void exec_once(Decode *s, vaddr_t pc) {
 
 static void execute(uint64_t n) {
   Decode s;
-  for (;n > 0; n --) {
+  for (; n > 0; n--) {
     exec_once(&s, cpu.pc);
-    g_nr_guest_inst ++;
+    g_nr_guest_inst++;
     trace_and_difftest(&s, cpu.pc);
-    if (nemu_state.state != NEMU_RUNNING) break;
+    if (nemu_state.state != NEMU_RUNNING)
+      break;
     IFDEF(CONFIG_DEVICE, device_update());
   }
 }
@@ -100,23 +159,32 @@ static void statistic() {
 #define NUMBERIC_FMT MUXDEF(CONFIG_TARGET_AM, "%", "%'") PRIu64
   Log("host time spent = " NUMBERIC_FMT " us", g_timer);
   Log("total guest instructions = " NUMBERIC_FMT, g_nr_guest_inst);
-  if (g_timer > 0) Log("simulation frequency = " NUMBERIC_FMT " inst/s", g_nr_guest_inst * 1000000 / g_timer);
-  else Log("Finish running in less than 1 us and can not calculate the simulation frequency");
+  if (g_timer > 0)
+    Log("simulation frequency = " NUMBERIC_FMT " inst/s",
+        g_nr_guest_inst * 1000000 / g_timer);
+  else
+    Log("Finish running in less than 1 us and can not calculate the simulation "
+        "frequency");
 }
 
 void assert_fail_msg() {
   isa_reg_display();
+  iringbuf_pop();
   statistic();
 }
 
 /* Simulate how the CPU works. */
 void cpu_exec(uint64_t n) {
-  g_print_step = (n < MAX_INST_TO_PRINT);
+  g_print_step =
+      (n < MAX_INST_TO_PRINT); //一次执行多余MAX_INST_TO_PRINT的指令就不会打印
   switch (nemu_state.state) {
-    case NEMU_END: case NEMU_ABORT:
-      printf("Program execution has ended. To restart the program, exit NEMU and run again.\n");
-      return;
-    default: nemu_state.state = NEMU_RUNNING;
+  case NEMU_END:
+  case NEMU_ABORT:
+    printf("Program execution has ended. To restart the program, exit NEMU and "
+           "run again.\n");
+    return;
+  default:
+    nemu_state.state = NEMU_RUNNING;
   }
 
   uint64_t timer_start = get_time();
@@ -127,15 +195,21 @@ void cpu_exec(uint64_t n) {
   g_timer += timer_end - timer_start;
 
   switch (nemu_state.state) {
-    case NEMU_RUNNING: nemu_state.state = NEMU_STOP; break;
+  case NEMU_RUNNING:
+    nemu_state.state = NEMU_STOP;
+    break;
 
-    case NEMU_END: case NEMU_ABORT:
-      Log("nemu: %s at pc = " FMT_WORD,
-          (nemu_state.state == NEMU_ABORT ? ANSI_FMT("ABORT", ANSI_FG_RED) :
-           (nemu_state.halt_ret == 0 ? ANSI_FMT("HIT GOOD TRAP", ANSI_FG_GREEN) :
-            ANSI_FMT("HIT BAD TRAP", ANSI_FG_RED))),
-          nemu_state.halt_pc);
-      // fall through
-    case NEMU_QUIT: statistic();
+  case NEMU_END:
+  case NEMU_ABORT:
+    Log("nemu: %s at pc = " FMT_WORD,
+        (nemu_state.state == NEMU_ABORT
+             ? ANSI_FMT("ABORT", ANSI_FG_RED)
+             : (nemu_state.halt_ret == 0
+                    ? ANSI_FMT("HIT GOOD TRAP", ANSI_FG_GREEN)
+                    : ANSI_FMT("HIT BAD TRAP", ANSI_FG_RED))),
+        nemu_state.halt_pc);
+    // fall through
+  case NEMU_QUIT:
+    statistic();
   }
 }
