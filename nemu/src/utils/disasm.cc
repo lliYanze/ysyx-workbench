9cf3c28f06c27705e8927dab2c4a739723358ce9
李彦泽 <@1310469009@qq.com>
2023-12-18 15:50:29 +0800

    处理完成 nemu接入difftest失败问题

--- nemu/src/utils/disasm.cc
+++ nemu/src/utils/disasm.cc
@@ -100,9 +100,11 @@ extern "C" void disassemble(char *str, int size, uint64_t pc, uint8_t *code,
   llvm::ArrayRef<uint8_t> arr(code, nbyte);
   uint64_t dummy_size = 0;
   gDisassembler->getInstruction(inst, dummy_size, arr, pc, llvm::nulls());
+
   std::string s;
   raw_string_ostream os(s);
   gIP->printInst(&inst, pc, "", *gSTI, os);
+
   int skip = s.find_first_not_of('\t');
   const char *p = s.c_str() + skip;
   assert((int)s.length() - skip < size);
