5039d65e77c7d085a225bcf5519976fd23868245
李彦泽 <@1310469009@qq.com>
2023-12-19 00:29:42 +0800

    nemu实现vga

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -1,25 +1,36 @@
 #include <am.h>
 #include <nemu.h>
+#include <stdio.h>
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
-void __am_gpu_init() {
-}
+void __am_gpu_init() {}
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
-  *cfg = (AM_GPU_CONFIG_T) {
-    .present = true, .has_accel = false,
-    .width = 0, .height = 0,
-    .vmemsz = 0
-  };
+  *cfg = (AM_GPU_CONFIG_T){.present = true,
+                           .has_accel = false,
+                           .width = inw(VGACTL_ADDR),
+                           .height = inl(VGACTL_ADDR) >> 16,
+                           .vmemsz = inl(FB_ADDR)};
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  if (ctl->sync) {
+  int x = ctl->x;
+  int y = ctl->y;
+  int h = ctl->h;
+  int w = ctl->w;
+  int screen_width = inl(VGACTL_ADDR) >> 16;
+  uint32_t *pixels = ctl->pixels;
+  if (!ctl->sync) {
+    uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+    for (int i = 0; i < h; ++i) { // 行
+      for (int j = 0; j < w; ++j) {
+        fb[screen_width * (i + x) + j + y] = pixels[w * i + h + j];
+      }
+    }
+  } else {
     outl(SYNC_ADDR, 1);
   }
 }
 
-void __am_gpu_status(AM_GPU_STATUS_T *status) {
-  status->ready = true;
-}
+void __am_gpu_status(AM_GPU_STATUS_T *status) { status->ready = true; }
--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -4,6 +4,11 @@
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  kbd->keydown = 0;
-  kbd->keycode = AM_KEY_NONE;
+  kbd->keycode = (int)inl(KBD_ADDR);
+  if (kbd->keycode & KEYDOWN_MASK) {
+    kbd->keydown = 1;
+    kbd->keycode = kbd->keycode ^ KEYDOWN_MASK;
+  } else {
+    kbd->keydown = 0;
+  }
 }
