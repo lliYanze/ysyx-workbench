2f7a0ea989e477e72f439091f61c0d9adc53c81d
李彦泽 <1310469009@qq.com>
2024-01-16 14:35:34 +0800

    适配yosys并且修整gitignore

--- npc/top/src/Alu.scala
+++ npc/top/src/Alu.scala
@@ -2,6 +2,7 @@ package Alu
 
 import instsinfo._
 import chisel3._
+import chisel3.util.MuxLookup
 
 class Alu extends Module {
   val io = IO(new Bundle {
@@ -21,52 +22,105 @@ class Alu extends Module {
     io.less := false.B
   }
 
-  when(io.op === OPCTL.ADD) {
-    dnotjump
-    io.out := io.s1 + io.s2
-  }.elsewhen(io.op === OPCTL.END) {
-    dnotjump
-    io.out := 0.U
-    io.end := true.B
-  }.elsewhen(io.op === OPCTL.NOP) {
-    dnotjump
-    io.out := 0.U
-  }.elsewhen(io.op === OPCTL.SUB) {
-    io.out := io.s1 - io.s2
-    dnotjump
-  }.elsewhen(io.op === OPCTL.SLT) {
-    io.out  := Mux(io.s1.asSInt < io.s2.asSInt, 1.U, 0.U)
-    io.eq   := (io.s1 - io.s2) === 0.U
-    io.less := io.s1.asSInt < io.s2.asSInt
-  }.elsewhen(io.op === OPCTL.SLTU) {
-    io.out  := Mux(io.s1 < io.s2, 1.U, 0.U)
-    io.eq   := io.out === 0.U
-    io.less := Mux(io.s1 < io.s2, 1.U, 0.U)
-  }.elsewhen(io.op === OPCTL.AND) {
-    dnotjump
-    io.out := io.s1 & io.s2
-  }.elsewhen(io.op === OPCTL.SLL) {
-    dnotjump
-    io.out := io.s1 << io.s2(4, 0)
-  }.elsewhen(io.op === OPCTL.SRL) {
-    dnotjump
-    io.out := io.s1 >> io.s2(4, 0)
-  }.elsewhen(io.op === OPCTL.SRA) {
-    dnotjump
-    io.out := (io.s1.asSInt >> io.s2(4, 0)).asUInt
-  }.elsewhen(io.op === OPCTL.LUI) {
-    dnotjump
-    io.out := io.s2
-  }.elsewhen(io.op === OPCTL.XOR) {
-    dnotjump
-    io.out := io.s1 ^ io.s2
-  }.elsewhen(io.op === OPCTL.OR) {
-    dnotjump
-    io.out := io.s1 | io.s2
-  }.otherwise {
-    dnotjump
-    io.out := 0.U
-    printf("Error: Unknown instruction! \n")
-    io.end := true.B
-  }
+  io.out := MuxLookup(
+    io.op,
+    0.U,
+    (
+      Seq(
+        OPCTL.ADD -> (io.s1 + io.s2),
+        OPCTL.END -> 0.U,
+        OPCTL.SUB -> (io.s1 - io.s2),
+        OPCTL.SLT -> Mux(io.s1.asSInt < io.s2.asSInt, 1.U, 0.U),
+        OPCTL.SLTU -> Mux(io.s1 < io.s2, 1.U, 0.U),
+        OPCTL.AND -> (io.s1 & io.s2),
+        OPCTL.SLL -> (io.s1 << io.s2(4, 0)),
+        OPCTL.SRL -> (io.s1 >> io.s2(4, 0)),
+        OPCTL.SRA -> (io.s1.asSInt >> io.s2(4, 0)).asUInt,
+        OPCTL.LUI -> io.s2,
+        OPCTL.XOR -> (io.s1 ^ io.s2),
+        OPCTL.OR -> (io.s1 | io.s2)
+      )
+    )
+  )
+
+  io.eq := MuxLookup(
+    io.op,
+    false.B,
+    (
+      Seq(
+        OPCTL.SLT -> ((io.s1 - io.s2) === 0.U),
+        OPCTL.SLTU -> (io.out === 0.U)
+      )
+    )
+  )
+
+  io.less := MuxLookup(
+    io.op,
+    false.B,
+    (
+      Seq(
+        OPCTL.SLT -> (io.s1.asSInt < io.s2.asSInt),
+        OPCTL.SLTU -> (io.s1 < io.s2)
+      )
+    )
+  )
+
+  io.end := MuxLookup(
+    io.op,
+    false.B,
+    (
+      Seq(
+        OPCTL.END -> true.B
+      )
+    )
+  )
+
+  // when(io.op === OPCTL.ADD) {
+  //   dnotjump
+  //   io.out := io.s1 + io.s2
+  // }.elsewhen(io.op === OPCTL.END) {
+  //   dnotjump
+  //   io.out := 0.U
+  //   io.end := true.B
+  // }.elsewhen(io.op === OPCTL.NOP) {
+  //   dnotjump
+  //   io.out := 0.U
+  // }.elsewhen(io.op === OPCTL.SUB) {
+  //   io.out := io.s1 - io.s2
+  //   dnotjump
+  // }.elsewhen(io.op === OPCTL.SLT) {
+  //   io.out  := Mux(io.s1.asSInt < io.s2.asSInt, 1.U, 0.U)
+  //   io.eq   := (io.s1 - io.s2) === 0.U
+  //   io.less := io.s1.asSInt < io.s2.asSInt
+  // }.elsewhen(io.op === OPCTL.SLTU) {
+  //   io.out  := Mux(io.s1 < io.s2, 1.U, 0.U)
+  //   io.eq   := io.out === 0.U
+  //   io.less := Mux(io.s1 < io.s2, 1.U, 0.U)
+  // }.elsewhen(io.op === OPCTL.AND) {
+  //   dnotjump
+  //   io.out := io.s1 & io.s2
+  // }.elsewhen(io.op === OPCTL.SLL) {
+  //   dnotjump
+  //   io.out := io.s1 << io.s2(4, 0)
+  // }.elsewhen(io.op === OPCTL.SRL) {
+  //   dnotjump
+  //   io.out := io.s1 >> io.s2(4, 0)
+  // }.elsewhen(io.op === OPCTL.SRA) {
+  //   dnotjump
+  //   io.out := (io.s1.asSInt >> io.s2(4, 0)).asUInt
+  // }.elsewhen(io.op === OPCTL.LUI) {
+  //   dnotjump
+  //   io.out := io.s2
+  // }.elsewhen(io.op === OPCTL.XOR) {
+  //   dnotjump
+  //   io.out := io.s1 ^ io.s2
+  // }.elsewhen(io.op === OPCTL.OR) {
+  //   dnotjump
+  //   io.out := io.s1 | io.s2
+  // }.otherwise {
+  //   dnotjump
+  //   io.out := 0.U
+  //   printf("Error: Unknown instruction! \n")
+  //   io.end := true.B
+  // }
 }
--- npc/top/src/Elaborate.scala
+++ npc/top/src/Elaborate.scala
@@ -1,11 +1,18 @@
 import circt.stage._
 
 object Elaborate extends App {
-  def top       = new TOP()
-  val useMFC    = false // use MLIR-based firrtl compiler
+  def top = new TOP()
+  val useMFC = false // use MLIR-based firrtl compiler
+  // val useMFC    = true // use MLIR-based firrtl compiler
   val generator = Seq(chisel3.stage.ChiselGeneratorAnnotation(() => top))
+  val firtoolOptions = Seq(
+    FirtoolOption("--lowering-options=disallowLocalVariables"),
+    FirtoolOption("--lowering-options=disallowPackedArrays"),
+    )
   if (useMFC) {
-    (new ChiselStage).execute(args, generator :+ CIRCTTargetAnnotation(CIRCTTarget.Verilog))
+    val lyz_generator = generator :+ CIRCTTargetAnnotation(CIRCTTarget.Verilog)
+    // (new ChiselStage).execute(args, generator :+ CIRCTTargetAnnotation(CIRCTTarget.Verilog))
+    (new ChiselStage).execute(args, lyz_generator ++ firtoolOptions)
   } else {
     (new chisel3.stage.ChiselStage).execute(args, generator)
   }
